8fc94a1b5d432129c6cd669a26ed277be10a6090
Roger Coll <roger.coll.aumatell@gmail.com>
2020-04-22 20:40:05 +0200

    For godoc and goreadme.herokuapp.com

--- feistel.go
+++ feistel.go
@@ -1,10 +1,11 @@
+// Package feistel provides the basic network to create chipers with provided functions
 package feistel
 
 import (
 	"fmt"
 )
 
-
+//Xor Bittwise operator between two int32 arrays
 func xor(a,b *[]rune) (*[]rune) {
 	c := make([]rune, len(*a))
 	for i := 0; i < len(*a); i++ {
@@ -13,6 +14,7 @@ func xor(a,b *[]rune) (*[]rune) {
 	return &c
 }
 
+//Splits a string into two equal parts and retuns it's ASCII values
 func splitString(a string) (*[]rune, *[]rune, error) {
 	//each character in rune corresponts to a int32 ASCII value, no need for a uint32
 	if len(a)%2 != 0 {
@@ -21,4 +23,9 @@ func splitString(a string) (*[]rune, *[]rune, error) {
 	l := []rune(a[:len(a)/2])
 	r := []rune(a[len(a)/2:])
 	return &l, &r, nil
+}
+
+// Main function which will allow a string and a crypthographic function as parameters
+func Run() error {
+	return nil
 }
